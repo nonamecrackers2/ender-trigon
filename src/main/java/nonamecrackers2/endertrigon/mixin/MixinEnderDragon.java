dc7d122f99a3b39f029fc41689e79ddd4f9893a8
nonamecrackers2 <nonamecrackers2@outlook.com>
2023-11-14 17:49:24 -0600

    Clean up

--- src/main/java/nonamecrackers2/endertrigon/mixin/MixinEnderDragon.java
+++ src/main/java/nonamecrackers2/endertrigon/mixin/MixinEnderDragon.java
@@ -20,14 +20,12 @@ import java.util.List;
 import java.util.function.Predicate;
 
 import org.apache.commons.lang3.ArrayUtils;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.world.entity.Entity;
